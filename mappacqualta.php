4bcc4d145e97dd37a058daaba8942963d5621329
Piersoft <piersoft2@gmail.com>
2013-12-06 00:35:11 +0100

    prima versione

---
+++ mappacqualta.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = 'http://paolomainardi.com:3050/api/devices?type=geo';
+$file = "mappa.json";
+$src = fopen($url, 'r');
+$dest = fopen($file, 'w');
+echo stream_copy_to_stream($src, $dest) . "";
+sleep(1);
+header("Location:http://www.apposta.biz/prove/mappacqualta.html");
+
+?>
+
